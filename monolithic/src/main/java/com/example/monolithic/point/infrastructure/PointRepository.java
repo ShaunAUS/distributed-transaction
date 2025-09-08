7bca6a61980b05016efcd9aa35e89f3c36cb12f3
ShaunAUS <wnsqud70@gmail.com>
2025-09-08 23:33:06 +0900

    feat(Point) : Point 구현

---
+++ monolithic/src/main/java/com/example/monolithic/point/infrastructure/PointRepository.java
@@ -0,0 +1,10 @@
+package com.example.monolithic.point.infrastructure;
+
+import com.example.monolithic.point.domain.Point;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PointRepository extends JpaRepository<Point, Long> {
+
+    Point findByUserId(Long userId);
+}
